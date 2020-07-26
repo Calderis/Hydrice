66dd231ebd2616df4862414780b1bb34093dabdf
Wetteren Rémi <remi.wetteren@hetic.net>
2020-07-26 14:30:14 +0200

    first commit

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5bda4ec52087abb7d94405554ee6c717'),
+    },
+  },
+});
